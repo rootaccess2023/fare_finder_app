52d340138b25a55932054db8ee10c1df7e5cf14c
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-07 14:15:33 +0100

    bugs: deleted unnecessary text in header

--- src/components/Details/DetailsHeader.tsx
+++ src/components/Details/DetailsHeader.tsx
@@ -10,12 +10,9 @@ export function DetailsHeader() {
     const { fareDetails } = context;
 
   return (
-    <header className="h-[53px] flex justify-between items-center bg-[#e6e6e9] px-3 font-light text-[#0063d3] lg:px-0">
-        <h1 className="flex items-center hover:text-[#003082] cursor-pointer">
-            {fareDetails ? "Details" : "Travel Information"}
-        </h1>
+    <header className="h-[53px] flex justify-center items-center bg-[#e6e6e9] px-3 font-light text-[#0063d3] lg:px-0 lg:justify-end">
         {fareDetails ? (
-        <div className="flex gap-4">
+        <div className="w-full flex justify-between gap-4 lg:justify-end">
             <button className="w-36 flex justify-end gap-2 hover:gap-1 cursor-pointer">
                 <span><CiSaveDown2 className="size-5" /></span>
                 <span>Save as image</span>
