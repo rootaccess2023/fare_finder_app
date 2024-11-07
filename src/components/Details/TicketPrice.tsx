f257da24030710c2078e268cd6d0150a4a9795e4
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-07 14:43:34 +0100

    details: added peso sign

--- src/components/Details/TicketPrice.tsx
+++ src/components/Details/TicketPrice.tsx
@@ -23,11 +23,11 @@ export function TicketPrice() {
                     </span>
                     <h1 className="relative font-visby text-4xl flex items-center font-light gap-2 py-1">
                     <span>
-                        {ticketType === "sjt" ? fareDetails.sjt_fare : fareDetails.svt_fare}
+                        PHP {ticketType === "sjt" ? fareDetails.sjt_fare : fareDetails.svt_fare}
                     </span>
-                        <span className="absolute left-12 top-1 text-2xl border-b-2 border-primary">.00</span>
+                        <span className="absolute left-[7.7rem] top-1 text-2xl border-b-2 border-primary">.00</span>
                     </h1>
-                    <span>single way, 2nd class, full fare, no subscription</span>
+                    <span>single way, standard, {ticketType === 'sjt' ? <span>full price</span> : <span>discounted price</span>}</span>
                 </div>
                 <div className="flex items-start justify-end text-tertiary font-light text-sm gap-2">
                 {ticketType === 'sjt' ? (
