1abcf6a1ae6f581cff4f07bf780452a233decd6c
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-09 13:44:23 +0100

    feat: added more projects

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Logo } from "./Header/Logo"
+import { Portfolio } from "./Header/Portfolio"
 
 
 export function Header() {
@@ -7,6 +8,7 @@ export function Header() {
     <header className="h-16 flex flex-col justify-center items-center bg-white lg:h-28">
       <div className="w-full flex justify-between items-center px-5 gap-2 max-w-6xl mx-auto lg:px-0">
         <Logo />
+        <Portfolio />
       </div>
     </header>
   )
