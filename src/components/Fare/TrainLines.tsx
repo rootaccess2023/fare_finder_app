32f5a61447522ccc279605efa7076027491beedf
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-09 13:07:37 +0100

    bugs: fixed bugs

--- src/components/Fare/TrainLines.tsx
+++ src/components/Fare/TrainLines.tsx
@@ -1,25 +1,32 @@
 import { useContext } from "react";
-import { PiTrainLight } from "react-icons/pi";
 import { stateContext } from "../../App";
-import { TrainLineProps } from "../../types/types";
+import { PiTrainLight } from "react-icons/pi";
+import { Line, TrainLineProps } from "../../types/types";
 
-export function TrainLines({setFromQuery, setToQuery}: TrainLineProps) {
-
+export function TrainLines({ setFromQuery, setToQuery }: TrainLineProps) {
   const context = useContext(stateContext);
   if (!context) return null;
-  const { handleFetchStations,setToggleButton } = context;
+  const { handleFetchStations, setToggleButton } = context;
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value as Line;
+    handleFetchStations(value);
+    setFromQuery("");
+    setToQuery("");
+    setToggleButton(true);
+  };
 
   return (
     <div className="w-fit flex gap-2 text-primary border-b-[0.125rem] border-[#0707211a] hover:border-primary pb-1 cursor-pointer sm:flex-row">
-        <h2 className="flex gap-2">
-            <PiTrainLight className="size-[23px]" />
-            <span>Train line:</span>
-        </h2>
-        <select className="bg-transparent">
-            <option onTouchStart={() => {handleFetchStations('lrt1'); setFromQuery(""); setToQuery(""); setToggleButton(true)}} onClick={() => { handleFetchStations('lrt1'); setFromQuery(""); setToQuery(""); setToggleButton(true) }} value="lrt1">LRT 1</option>
-            <option onTouchStart={() => {handleFetchStations('lrt2'); setFromQuery(""); setToQuery(""); setToggleButton(true)}} onClick={() => { handleFetchStations('lrt2'); setFromQuery(""); setToQuery(""); setToggleButton(true) }} value="lrt2">LRT 2</option>
-            <option onTouchStart={() => {handleFetchStations('mrt3'); setFromQuery(""); setToQuery(""); setToggleButton(true)}} onClick={() => { handleFetchStations('mrt3'); setFromQuery(""); setToQuery(""); setToggleButton(true) }} value="mrt3">MRT 3</option>
-        </select>
+      <h2 className="flex gap-2">
+        <PiTrainLight className="size-[23px]" />
+        <span>Train line:</span>
+      </h2>
+      <select className="bg-transparent" onChange={handleSelectChange}>
+        <option value="lrt1">LRT 1</option>
+        <option value="lrt2">LRT 2</option>
+        <option value="mrt3">MRT 3</option>
+      </select>
     </div>
-  )
+  );
 }
